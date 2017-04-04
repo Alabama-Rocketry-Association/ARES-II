54e1f76335927fcfc45c214505500c4964c5c709
Spencer McArthur <Spencer@Spencers-MacBook-Pro.local>
2017-04-03 19:33:12 -0500

    Updated parsing.ino

--- Arduino/GPSlogger/parsing/parsing.ino
+++ Arduino/GPSlogger/parsing/parsing.ino
@@ -1,72 +1,32 @@
-// Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
-//
-// This code shows how to listen to the GPS module in an interrupt
-// which allows the program to have more 'freedom' - just parse
-// when a new NMEA sentence is available! Then access data when
-// desired.
-//
-// Tested and works great with the Adafruit Ultimate GPS module
-// using MTK33x9 chipset
-//    ------> http://www.adafruit.com/products/746
-// Pick one up today at the Adafruit electronics shop 
-// and help support open source hardware & software! -ada
-
-//This code is intended for use with Arduino Leonardo and other ATmega32U4-based Arduinos
-
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 
-// Connect the GPS Power pin to 5V
-// Connect the GPS Ground pin to ground
-// If using software serial (sketch example default):
-//   Connect the GPS TX (transmit) pin to Digital 8
-//   Connect the GPS RX (receive) pin to Digital 7
-// If using hardware serial:
-//   Connect the GPS TX (transmit) pin to Arduino RX1 (Digital 0)
-//   Connect the GPS RX (receive) pin to matching TX1 (Digital 1)
-
-// If using software serial, keep these lines enabled
-// (you can change the pin numbers to match your wiring):
 SoftwareSerial mySerial(8, 7);
 Adafruit_GPS GPS(&mySerial);
 
-// If using hardware serial, comment
-// out the above two lines and enable these two lines instead:
-//Adafruit_GPS GPS(&Serial1);
-//HardwareSerial mySerial = Serial1;
-
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences
 #define GPSECHO  false
 
 void setup()  
 {
-    
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
   Serial.begin(115200);
-  delay(5000);
-  Serial.println("Adafruit GPS library basic test!");
+  // delay(5000);
 
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
   GPS.begin(9600);
   
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  // RMC (recommended minimum) and GGA (fix data) including altitude
   //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
+  // "minimum recommended" data
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
   
   // Set the update rate
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
+  GPS.sendCommand(PMTK_API_SET_FIX_CTL_1HZ); // 1 Hz position fix update rate
 
   // Request updates on antenna status, comment out to keep quiet
   GPS.sendCommand(PGCMD_ANTENNA);
 
-  delay(1000);
+  // delay(1000);
   // Ask for firmware version
   mySerial.println(PMTK_Q_RELEASE);
 }
@@ -81,43 +41,40 @@ void loop()                     // run over and over again
   
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences! 
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
     //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
   
     if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
+      return; // we can fail to parse a sentence in which case we should just wait for another
   }
 
   // if millis() or timer wraps around, we'll just reset it
   if (timer > millis())  timer = millis();
 
   // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) { 
+  if (millis() - timer > 1000) { 
     timer = millis(); // reset the timer
     
-    Serial.print("\nTime: ");
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
     if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      float intPartLat = floor(GPS.latitude / 100.0);
+      float intPartLon = floor(GPS.longitude / 100.0);
+      float decPartLat = ( (GPS.latitude / 100.0) - intPartLat ) / 0.6;
+      float decPartLon = ( (GPS.longitude / 100.0) - intPartLon ) / 0.6;
+      GPS.latitude =  intPartLat + decPartLat;
+      GPS.longitude = intPartLon + decPartLon;
+      if (GPS.lat == 'S') GPS.latitude *= -1.0;
+      if (GPS.lon == 'W') GPS.longitude *= -1.0;
+      Serial.print(GPS.latitude, 4); // Serial.print(GPS.lat);
       Serial.print(", "); 
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.println(GPS.longitude, 4); // Serial.print(GPS.lon);
       
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      /*Serial.print(", ");
+      Serial.print(GPS.hour, DEC); Serial.print(':');
+      Serial.print(GPS.minute, DEC); Serial.print(':');
+      Serial.print(GPS.seconds, DEC); Serial.print('.');
+      Serial.println(GPS.milliseconds);*/
+      
+      // Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      // Serial.print("Altitude: "); Serial.println(GPS.altitude);
     }
   }
 }
